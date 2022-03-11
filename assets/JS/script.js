c1ca8d440d8f5d6e2f8151e9c4c10aeaf5a87774
Leo Wong <leoelicos@gmail.com>
2022-03-12 01:23:39 +1100

    Updated link

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -153,6 +153,6 @@ function recordSection() {
 		SYTYCJ === null ? (SYTYCJ = []) : (SYTYCJ = JSON.parse(SYTYCJ));
 		SYTYCJ.push({ initials: initials, score: secondsLeft });
 		localStorage.setItem('SYTYCJ', JSON.stringify(SYTYCJ));
-		window.location.replace('../JS/hs.html');
+		window.location.replace('./assets/JS/hs.html');
 	});
 }
